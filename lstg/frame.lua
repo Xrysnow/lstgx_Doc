d0bf93f8337a3691a329eadeb2ef9b8003553396
Xrysnow <xrysnow@outlook.com>
2019-03-31 16:33:19 +0800

    Update frame.lua

--- lstg/frame.lua
+++ lstg/frame.lua
@@ -20,34 +20,16 @@ end
 function SetVsync(boolean)
 end
 
----写出日志。
----@param string string
----@return void
-function SystemLog(string)
+
+---向日志文件写入字符串
+---@param str string
+function SystemLog(str)
+    lstg.LogSystem:getInstance():writeLine('[LOG] '.. str)
 end
 
 ---将若干值写到日志。
 function Print(...)
 end
-
----加载指定位置的ZIP资源包，可选填密码。
----失败将导致错误。
---- 细节
----   后加载的资源包有较高的查找优先级。这意味着可以通过该机制加载资源包来覆盖基础资源包中的文件。
----   一旦zip文件被打开，将不能被访问。
----   加载文件时将按照优先级依次搜索资源包，若资源包中不含文件则从当前目录加载。
---function LoadPack(path, password)
---end
-
----卸载指定位置的资源包，要求路径名必须一致。
----若资源包不存在不发生错误。
---function UnloadPack(path)
---end
-
----将资源包中的数据解压到本地。
----若失败将抛出错误。
---function ExtractRes(path, target)
---end
 
 ---执行指定路径的脚本。已执行过的脚本会再次执行。
 ---若文件不存在、编译失败、执行失败则抛出错误。
