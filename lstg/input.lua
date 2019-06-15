055f59254d9eda233a9279f06290e54f1d9277de
Xrysnow <xrysnow@outlook.com>
2019-06-15 17:49:41 +0800

    update lstg/input.lua

--- lstg/input.lua
+++ lstg/input.lua
@@ -4,18 +4,6 @@
 --- Copyright (C) 2018-2019 Xrysnow. All rights reserved.
 ---
 
-
----立即更新输入状态
---function UpdateInput()
---end
-
----给出虚拟键代码检测是否按下
----    VK_CODE的具体含义请查阅MSDN
----@param vk_code number
----@return boolean
---function GetKeyState(vk_code)
---end
-
 ---返回最后一次输入的按键的虚拟键代码
 ---@return number
 --function GetLastKey()
@@ -29,11 +17,12 @@ end
 --
 
 ---获取鼠标的位置，以窗口左下角为原点
+---@return number,number
 function GetMousePosition()
 end
 
 ---检查鼠标按键是否按下
----@param button number @可取0、1、2，分别对应鼠标左键、中键、右键
+---@param button number 可取0、1、2，分别对应鼠标左键、中键、右键
 ---@return boolean
 function GetMouseState(button)
 end
